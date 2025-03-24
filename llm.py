f11a0df4e4e4cf19e49b1e791874843afec53941
aliasad059 <aliasad059@gmail.com>
2025-03-24 10:11:00 -0400

    feat: Add support for additional AI models including Deepseek, Gemini and Grok models

--- llm.py
+++ llm.py
@@ -2,6 +2,7 @@ import openai
 import os
 from fireworks.client import Fireworks
 import yaml
+import google.generativeai as genai
 
 with open('config.yml', 'r') as file:
     try:
@@ -11,8 +12,12 @@ with open('config.yml', 'r') as file:
 
 fw_key = config["llm_config"]["fw_key"]
 openai.api_key = config["llm_config"]["oai_key"]
+gemini_key = config["llm_config"]["gemini_key"]
+deepseek_key = config["llm_config"]["deepseek_key"]
+xai_key = config["llm_config"]["xai_key"]
 
 
+genai.configure(api_key=gemini_key)
 
 def getGPT4oMiniResponse(prompt, system_prompt, model="gpt-4o-mini", max_tokens=500):
     response = openai.ChatCompletion.create(
@@ -65,10 +70,51 @@ def getLlama3p1Response(prompt, system_prompt):
     return response.message.content
 
 
+def getDeepSpeekR1Response(prompt, system_prompt, model='deepseek-reasoner'):
+     # see models options at https://api-docs.deepseek.com/quick_start/pricing
+    response = openai.ChatCompletion.create(
+            model=model,
+            openai_api_base='https://api.deepseek.com',
+            openai_api_key=deepseek_key,
+            messages=[
+                {"role": "system", "content": f"{system_prompt}"},
+                {"role": "user", "content": f"{prompt}"}
+            ],
+        ).choices[0]
+
+    return response.message.content
+
+
+def getGrok2Response(prompt, system_prompt, model='grok-2-latest'):
+     # see models options at https://docs.x.ai/docs/models?cluster=us-east-1#models-and-pricing
+    response = openai.ChatCompletion.create(
+            model=model,
+            openai_api_base='https://api.x.ai/v1',
+            openai_api_key=xai_key,
+            messages=[
+                {"role": "system", "content": f"{system_prompt}"},
+                {"role": "user", "content": f"{prompt}"}
+            ],
+        ).choices[0]
+
+    return response.message.content
+
+
+def getGemini2FlashResponse(prompt, system_prompt, model="gemini-2.0-flash-001"):
+    # see models options at https://cloud.google.com/vertex-ai/generative-ai/docs/model-reference/inference#supported-models
+    model_gen = genai.GenerativeModel(model)
+    chat = model_gen.start_chat(history=[])
+    response = chat.send_message(f"{system_prompt}\n{prompt}")
+    return response.text
+
+
 
 llmResponseGetters = {
     "4o-mini": getGPT4oMiniResponse,
     "o1-mini": getO1MiniResponse,
     "o3-mini": getO3MiniResponse,
     "llama-3p1": getLlama3p1Response,
+    "deepseek-reasoner": getDeepSpeekR1Response,
+    "grok-2-latest": getGrok2Response,
+    "gemini-2.0-flash-001": getGemini2FlashResponse,
 }
