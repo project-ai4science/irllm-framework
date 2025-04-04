fec52b001fbce1917caa5f48d16c6568a6241479
沈元皓 <ys3426@nyu.edu>
2025-04-04 03:09:20 -0400

    v3 of the framework with separate branch

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ CONFIG = load_config(config_path)
 def main():
     task_config = CONFIG['task_config']
     assert args.task in task_config['task_list']
-    save_path = args.save_path if args.save_path else task_config.get('save_path', './output') 
     
     # map the args to actual task
     if args.task == "output_evaluation":
         eval_type = args.eval_type
         evaluator = Evaluator(type=eval_type)
     else:
-        task_handler = TaskHandler(provider=args.provider, model_name=args.model_name, lm_config_path=config_path, save_path=save_path)
+        # task_handler = TaskHandler(provider=args.provider, model_name=args.model_name, lm_config_path=config_path, save_path=save_path, **task_config)
+        task_handler = TaskHandler(provider=args.provider, model_name=args.model_name, lm_config_path=config_path, **task_config)
         task_func = task_handler[args.task]
         task_func()
 
@@ -29,6 +29,7 @@ if __name__ == "__main__":
     parser.add_argument("--task", type=str, help="Task type (e.g., classification, recommendation, generation)", required=True)
     parser.add_argument("--eval_type", type=str, help="Choose the type of result evalutaion for different tasks", default=None)
 
-    parser.add_argument("--save_path", type=str, help="subfolder to save the results", default=None)
+    # parser.add_argument("--save_path", type=str, help="subfolder to save the experiment results", default=None)
+    # parser.add_argument("--out_path", type=str, help="subfolder to save the evaluation results", default=None)
     args = parser.parse_args()
     main()
